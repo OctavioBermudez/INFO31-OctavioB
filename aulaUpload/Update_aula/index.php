48a5c4e4a58aead02dce92a6d69ea881ad895bd1
Octavio Bermudez <octaviobermudez825@gmail.com>
2026-04-06 09:27:54 -0300

    Create index.php
    Arquivos da aula de upload

---
+++ aulaUpload/Update_aula/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H2>Enviar arquivo</H2>
+        <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <input type="file" name="arquivo">   
+            <br>
+            <button type = "submit"> Enviar</button> 
+
+
+        </form>
+</body>
+</html>
